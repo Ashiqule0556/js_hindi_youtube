c5c75ff422d4b559257095429e71283459e377fa
Ashiqule0556 <123960166+Ashiqule0556@users.noreply.github.com>
2025-03-18 21:04:23 +0000

    conversion

---
+++ conversionOperation/conversion.js
@@ -0,0 +1,22 @@
+let score="33abvc";
+
+
+console.log(score);
+console.log(typeof score)
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1=true 0=false
+//""=false
+//"ashiqule"=true;
+
+let someNumber=33;
+let numbertoString=String(someNumber);
+console.log(numbertoString);
+console.log(typeof numbertoString)
+con
