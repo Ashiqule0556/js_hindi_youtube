3b2ef2210ce9ef69b159572e1aabe7a87937cdac
Ashiqule0556 <123960166+Ashiqule0556@users.noreply.github.com>
2025-03-18 20:12:15 +0000

    data_type

---
+++ 01_variable/variable.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="ashiqule@gmail.com";
+var accountPassword="12345";
+accountCity="Jaipur";
+let city;
+
+//accountId=452   // not allowed
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ 
+*/
+
+accountEmail="abc@gmail.com"
+accountCity="kolkata"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,city]);
+
+
---
+++ data_type/data_type.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+//alert(3+4);  //we are using nodejs, not browser
+
+
+console.log(2+
+    3
+)
+// code readability should be high
+let name="ashiqule"
+let age=21
+let isLoggedIn=false
+//number=>2 to power 53
+//bigint
+//string="" 
+//boolean=true/false
+// null=standalone value
+//undefined= 
+//symbol= unique
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);// output undefined
+console.log(typeof null); //output object
+
